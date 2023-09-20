ba7f3d8e7578ad0b33bcb7a82139f5691861832a
Mateo Laforge <lfm4179a@ens.fsi>
2023-09-20 17:48:57 +0200

    Fin TP1

--- src/fr/loferga/jeu/Sabot.java
+++ src/fr/loferga/jeu/Sabot.java
@@ -1,8 +1,13 @@
 package fr.loferga.jeu;
 
-import fr.loferga.Carte;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Sabot {
+import fr.loferga.Carte;
+import fr.loferga.Parade;
+import fr.loferga.Probleme.Type;
+
+public class Sabot implements Iterable<Carte> {
 	
 	private Carte[] cartes;
 	private int nbCartes;
@@ -33,6 +38,67 @@ public class Sabot {
 		for (int i = 0; i<cartes.length; i++) {
 			ajouterFamilleCarte(cartes[i]);
 		}
+	}
+	
+
+	
+	@Override
+	public Iterator<Carte> iterator() {
+		return new Iterator<Carte>() {
+			
+			private int i = -1;
+
+			@Override
+			public boolean hasNext() {
+				return i<nbCartes-1;
+			}
+
+			@Override
+			public Carte next() {
+				if (!hasNext()) {
+					throw new NoSuchElementException();
+				}
+				i++;
+				return cartes[i];
+			}
+			
+			@Override
+			public void remove() {
+				if (i<0 || i>nbCartes) {
+					throw new UnsupportedOperationException();
+				}
+				for (int j = i; j<nbCartes-1; j++) {
+					cartes[j] = cartes[j+1];
+				}
+				nbCartes--;
+				i--;
+			}
+			
+		};
+	}
+	
+	public Carte piocher() {
+		Iterator<Carte> it = iterator();
+		Carte toReturn = it.next();
+		it.remove();
+		return toReturn;
+	}
+	
+	// Test
+	public void print() {
+		for (int i = 0; i<nbCartes; i++)
+			System.out.println(cartes[i]);
+	}
+	
+	public static void main(String[] args) {
+		Sabot sabot = new Sabot(110);
+		sabot.ajouterFamilleCarte(new Parade(6, Type.ACCIDENT));
+		System.out.println("-- BEFORE --");
+		sabot.print();
+		System.out.println("-- PIOCHER --");
+		System.out.println(sabot.piocher());
+		System.out.println("-- AFTER --");
+		sabot.print();
 	}
 	
 }
---
+++ src/fr/loferga/testFonctionnels/Test.java
@@ -0,0 +1,27 @@
+package fr.loferga.testFonctionnels;
+
+import java.util.Iterator;
+
+import fr.loferga.Attaque;
+import fr.loferga.Botte;
+import fr.loferga.Carte;
+import fr.loferga.Parade;
+import fr.loferga.Probleme.Type;
+import fr.loferga.jeu.Sabot;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Sabot sabot = new Sabot(110);
+		sabot.ajouterFamilleCarte(
+				new Attaque(3, Type.ACCIDENT),
+				new Parade(3, Type.ACCIDENT),
+				new Botte(1, Type.ACCIDENT)
+				);
+		Iterator<Carte> it = sabot.iterator();
+		while (it.hasNext()) {
+			System.out.println("je pioche " + sabot.piocher());			
+		}
+	}
+	
+}
