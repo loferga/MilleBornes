f002f6f6c2aa1520a3b251f7c0eeda411a9553ab
loferga <laforgemateo@gmail.com>
2023-11-16 20:23:36 +0100

    remove useless imports

--- src/fr/loferga/utils/Utils.java
+++ src/fr/loferga/utils/Utils.java
@@ -2,7 +2,6 @@ package fr.loferga.utils;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
